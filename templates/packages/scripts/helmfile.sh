e54bac07b2fc918a70c8faf9e36f0b3d6efb5d9c
Erhhung Yuan <erhhung@gmail.com>
2025-11-02 00:04:32 -0700

    Chore: install GitLab Helm chart using Helmfile with Kustomize
    Signed-off-by: Erhhung Yuan <erhhung@gmail.com>

---
+++ templates/packages/scripts/helmfile.sh
@@ -0,0 +1,17 @@
+# https://github.com/helmfile/helmfile
+
+# shellcheck disable=SC2148 # Tips depend on target shell
+
+set -eo pipefail
+
+REL="https://github.com/helmfile/helmfile/releases/latest"
+VER=$(curl -Is $REL | sed -En 's/^location:.+\/tag\/v(.+)\r$/\1/p')
+
+# check if latest version already installed
+command -v helmfile &> /dev/null && {
+  ver=$(helmfile version -o short 2> /dev/null)
+  [ "$ver" == "$VER" ] && exit 9 # no change
+}
+ARCH=$(uname -m | sed -e 's/aarch64/arm64/' -e 's/x86_64/amd64/')
+curl -fsSL "$REL/download/helmfile_${VER}_linux_$ARCH.tar.gz" | \
+  tar -xz -C /usr/local/bin --no-same-owner helmfile
---
+++ templates/packages/scripts/vals.sh
@@ -0,0 +1,20 @@
+# https://github.com/helmfile/vals
+
+# shellcheck disable=SC2148 # Tips depend on target shell
+# shellcheck disable=SC2006 # Prefer $(...) over legacy `...`
+# shellcheck disable=SC2207 # Prefer mapfile to split output
+# shellcheck disable=SC2086 # Double quote prevent globbing
+
+set -eo pipefail
+
+REL="https://github.com/helmfile/vals/releases/latest"
+VER=$(curl -Is $REL | sed -En 's/^location:.+\/tag\/v(.+)\r$/\1/p')
+
+# check if latest version already installed
+command -v vals &> /dev/null && {
+  ver=$(v=(`vals version`); echo ${v[1]})
+  [ "$ver" == "$VER" ] && exit 9 # no change
+}
+ARCH=$(uname -m | sed -e 's/aarch64/arm64/' -e 's/x86_64/amd64/')
+curl -fsSL "$REL/download/vals_${VER}_linux_$ARCH.tar.gz" | \
+  tar -xz -C /usr/local/bin --no-same-owner vals
