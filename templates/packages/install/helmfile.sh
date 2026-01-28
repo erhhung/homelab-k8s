cea3147b8103da44418dca47d979fc653dc6f554
Erhhung Yuan <erhhung@gmail.com>
2026-01-28 01:34:32 -0800

    ✨ Feat: use MCP tool servers in Open WebUI via MCPO proxy
    Signed-off-by: Erhhung Yuan <erhhung@gmail.com>

--- templates/packages/install/helmfile.sh
+++ templates/packages/install/helmfile.sh
@@ -15,3 +15,41 @@ command -v helmfile &> /dev/null && {
 ARCH=$(uname -m | sed -e 's/aarch64/arm64/' -e 's/x86_64/amd64/')
 curl -fsSL "$REL/download/v${VER}/helmfile_${VER}_linux_${ARCH}.tar.gz" | \
   tar -xz -C /usr/local/bin --no-same-owner helmfile
+
+# create wrapper script around `helm` for use
+# by Helmfile to run custom commands prior to
+# certain Helm actions
+SCRIPT="/usr/local/bin/helm.sh"
+cat <<'EOF' > $SCRIPT
+#!/usr/bin/env bash
+set -eo pipefail
+
+get_chart_dir() {
+  while [ "$1" ]; do
+    [[ "$1" != -* && -d "$1" ]] && {
+      echo "$1"
+      return 0
+    } || shift
+  done
+}
+
+if [[ "$1" =~ ^(template|install|upgrade)$ ]]; then
+  # run custom commands if hook
+  # provided as environment var
+  hook_env="HELM_${1^^}_HOOK"
+
+  if [ "${!hook_env}" ]; then
+    chart_dir=$(get_chart_dir "${@:2}")
+    [ "$chart_dir" ] || {
+      echo >&2 -e "Unable to determine chart directory from Helm command:\n$0 $*"
+      exit 1
+    }
+    export CHART_DIR=$chart_dir
+    # run in isolated subshell
+    (eval "${!hook_env}") >&2
+  fi
+fi
+
+exec helm "$@"
+EOF
+chmod +x $SCRIPT
