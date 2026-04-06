54d440b85c6e87a1ff8aada36f9b3e9e2bb258b9
Erhhung Yuan <erhhung@gmail.com>
2026-04-05 22:16:05 -0700

    ⚡️ Perf: optimized init container speed; exec tool works but not sent to node
    Signed-off-by: Erhhung Yuan <erhhung@gmail.com>

--- files/openclaw/mac/ai.openclaw.macos.sh
+++ files/openclaw/mac/ai.openclaw.macos.sh
@@ -1,34 +1,68 @@
 #!/usr/bin/env bash
+
+# shellcheck disable=SC2086 # Double quote prevent globbing
+# shellcheck disable=SC2128 # Expanding array without index
+# shellcheck disable=SC2207 # Prefer mapfile to split output
+# shellcheck disable=SC2329 # This function is never invoked
+
 set -eo pipefail
+
+# strip punctuations, replace spaces
+# with dashes, convert to lower case
+MAC_NODE_ID=$(scutil --get ComputerName | \
+  perl -C -pe 's/[\p{P}\p{S}]//g;
+               s/\s+/-/g;
+               s/^-|-$//g;
+               $_ = lc($_)')
+GATEWAY_HOST="openclaw.fourteeners.local"
+GATEWAY_PORT="443"
 
 APP="/Applications/OpenClaw.app/Contents/MacOS/OpenClaw"
 # npm install --global openclaw
 # installs into /usr/local/bin
 CLI="/usr/local/bin/openclaw"
 
-# launch the Mac companion app, which fails
-# to register with remote gateway as a node:
-# https://github.com/openclaw/openclaw/issues/57243
-$APP 2> /dev/null &
-app_pid=$!
-
-$CLI node run --host openclaw.fourteeners.local --port 443 --tls &
-node_pid=$!
+kill_pgroup() {
+  local pgid
+  pgid=($(ps -o pgid= -p $1  2> /dev/null)) || return 0
+  [ "$pgid" ] && kill -$pgid 2> /dev/null   || true
+}
 
 cleanup() {
-  kill $app_pid $node_pid 2> /dev/null || true
+  (( cleaning )) && return 0
+  cleaning=1
+
+  kill_pgroup  $app_pid
+  kill_pgroup $node_pid
+
   wait $app_pid $node_pid 2> /dev/null || true
 }
 trap cleanup EXIT INT TERM
 
-while :; do
-  kill -0 $app_pid 2> /dev/null || {
-    echo "OpenClaw app exited; stopping node"
-    exit 1
-  }
-  kill -0 $node_pid 2> /dev/null || {
-    echo "openclaw-node exited; stopping app"
-    exit 1
-  }
-  sleep 1
-done
+# launch the Mac companion app, which fails
+# to register with remote gateway as a node:
+# https://github.com/openclaw/openclaw/issues/57243
+# setsid: brew install util-linux (append to
+# PATH: $HOMEBREW_PREFIX/opt/util-linux/bin)
+setsid $APP 2> /dev/null &
+app_pid=$!
+
+# --node-id is supposed to override the device
+# ID, but, in practice, the node is still only
+# addressable by its display name & device ID:
+# https://github.com/openclaw/openclaw/issues/61569
+setsid $CLI node run \
+  --node-id $MAC_NODE_ID  \
+  --host    $GATEWAY_HOST \
+  --port    $GATEWAY_PORT \
+  --tls  &
+node_pid=$!
+
+wait -n $app_pid $node_pid || true
+
+if ! kill -0 $app_pid 2> /dev/null; then
+  echo "OpenClaw app exited; stopping node"
+elif ! kill -0 $node_pid 2> /dev/null; then
+  echo "openclaw-node exited; stopping app"
+fi
+exit 1
