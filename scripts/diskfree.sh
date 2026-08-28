07c927ff75fc7f8b11f2d3cb479ba744ad9d712a
Erhhung Yuan <erhhung@gmail.com>
2026-08-28 10:22:38 -0700

    ⚡️ Fix: limit Longhorn replica rebuilding concurrency per node; add iostst & mpstat on nodes
    Signed-off-by: Erhhung Yuan <erhhung@gmail.com>

--- scripts/diskfree.sh
+++ scripts/diskfree.sh
@@ -4,12 +4,11 @@
 # nodes (assumes `ssh <host>` works without
 # password for hosts: rancher & k8s1..k8sN)
 
-# shellcheck disable=SC2059 # Don't use vars in printf format
+# shellcheck disable=SC2086 # Double quote prevent globbing
 # shellcheck disable=SC2207 # Prefer mapfile to split output
+# shellcheck disable=SC2059 # Don't use vars in printf format
 
 set -o pipefail
-
-kubectl="kubectl --context homelab"
 
 K8S_HOSTS=(rancher $(seq -f "k8s%g" 1 6))
 DF_MOUNTS=(/ /data)
@@ -50,7 +49,7 @@ title() {
 }
 
 echo -en "\n${LTCYAN}Gathering disk information...${NOCLR}"
-last_host=
+unset last_host
 
 while IFS= read -r line; do
   # parallel --tag prefixes each
@@ -76,33 +75,54 @@ done < <(
 # show available storage from Longhorn perspective
 # "allocatable" is size available for provisioning
 
-COLS=(node disk capacity allocatable available scheduled reserved)
-FMTS='%-4s  %-25s  %8s  %11s  %9s  %9s  %8s'
-printf -v header "$FMTS" "${COLS[@]^^}"
-echo; title "${YELLOW}$header${NOCLR}"
+echo -en "\n${LTCYAN}Gathering Longhorn information...${NOCLR}"
+kubectl="kubectl --context homelab -n longhorn-system -o json"
 
-while IFS=$'\t' read -r node disk capacity \
-    allocatable available scheduled reserved; do
-  sizes=($(numfmt --to=iec "$capacity" "$allocatable" \
-           "$available" "$scheduled" "$reserved"))
-  printf "$FMTS\n" "$node" "$disk" "${sizes[@]}"
+COLS=(node replicas disk capacity
+  allocatable available scheduled reserved)
+FMTS='%-4s  %8s  %-25s  %8s  %11s  %9s  %9s  %8s'
+printf -v header "$FMTS" "${COLS[@]^^}"
+
+while IFS=$'\t' read -r node replicas disk capacity \
+         allocatable available scheduled reserved; do
+  [ "$header" ] && {
+    printf '\r\033[2K'
+    title "${YELLOW}$header${NOCLR}"
+    unset header
+  }
+  sizes=($(numfmt --to=iec $capacity $allocatable \
+                   $available $scheduled $reserved))
+  printf "$FMTS\n" $node $replicas $disk "${sizes[@]}"
 done < <(
-  $kubectl get nodes.longhorn.io -n longhorn-system -o json | \
-    jq -r '.items[] as $node
-      |  $node.metadata.name            as $name
-      | ($node.spec.disks        // {}) as $specdisks
-      | ($node.status.diskStatus // {})
-      |  to_entries[]
-      | .key as $disk
-      | ($specdisks[$disk].storageReserved // 0) as $reserved
-      | (.value.storageMaximum             // 0) as $capacity
-      | (.value.storageAvailable           // 0) as $available
-      | (.value.storageScheduled           // 0) as $scheduled
-      | ($capacity - $reserved - $scheduled)     as $schedulable
-      | (if   $available    < $schedulable
-         then $available else $schedulable end)  as $allocatable
-      | [$name, $disk, $capacity, $allocatable,
-          $available, $scheduled, $reserved]
-      | @tsv
-    '
+  $kubectl get nodes.longhorn.io | \
+    jq --slurpfile replicaList <(
+      $kubectl get replicas.longhorn.io
+      ) -r '(
+        $replicaList[0].items
+        | reduce .[] as $replica ({};
+            if $replica.metadata.deletionTimestamp == null
+              and ($replica.spec.nodeID // "") != ""
+            then .[$replica.spec.nodeID] += 1
+            else .
+            end
+          )
+        ) as $replicaCounts
+        | .items[] as $node
+        |  $node.metadata.name            as $name
+        | ($node.spec.disks        // {}) as $specdisks
+        | ($node.status.diskStatus // {})
+        |  to_entries[]
+        | .key as $disk
+        | ($specdisks[$disk].storageReserved // 0) as $reserved
+        | ($replicaCounts[$name]             // 0) as $replicas
+        | (.value.storageMaximum             // 0) as $capacity
+        | (.value.storageAvailable           // 0) as $available
+        | (.value.storageScheduled           // 0) as $scheduled
+        | ($capacity - $reserved - $scheduled)     as $schedulable
+        | (if   $available    < $schedulable
+          then $available else $schedulable end)  as $allocatable
+        | [$name, $replicas, $disk, $capacity,
+          $allocatable, $available, $scheduled, $reserved]
+        | @tsv
+      '
 )
